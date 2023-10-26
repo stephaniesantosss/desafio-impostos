f604bd7c0d9d1f54a5e5fdfd0724ad3568e50151
stephaniesantosss <stephanieps2016@hotmail.com>
2023-10-26 12:10:30 -0300

    Configuracao inicial

---
+++ src/main/java/com/desafio/impostos/ImpostosApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.impostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/impostos/ImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.impostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
